e228ed1a81a3efbddf8be65fb4d2caa2be66064b
Petr Šrámek <petr.sramek@cloudikka.com>
2017-12-16 11:52:37 -0800

    Update PolylineDecoderBase.cs

--- src/Cloudikka.PolylineAlgorithm/Cloudikka.PolylineAlgorithm/PolylineDecoderBase.cs
+++ src/Cloudikka.PolylineAlgorithm/Cloudikka.PolylineAlgorithm/PolylineDecoderBase.cs
@@ -1,7 +1,7 @@
 ﻿//  
 // Copyright (c) Petr Šrámek. All rights reserved.  
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.  
-// 
+//
 
 namespace Cloudikka.PolylineAlgorithm {
 	using System;
